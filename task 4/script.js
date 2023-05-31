59886580bc949c1ef2e67470b20f720a5f8009cd
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-05-31 19:31:45 +0300

    update task 4/script.js

---
+++ task 4/script.js
@@ -0,0 +1,12 @@
+// Визначити середнє арифметичне трьох чисел
+
+let number1 = parseInt(prompt("Введіть перше число:"));
+let number2 = parseInt(prompt("Введіть перше число:"));
+let number3 = parseInt(prompt("Введіть перше число:"));
+
+if (Number.isNaN(number1) || Number.isNaN(number2) || Number.isNaN(number3)) {
+  alert("Спробуйте ще...");
+} else {
+  const mid = (number1 + number2 + number3) / 3;
+  alert(`Середне арифметичне: ${mid}`);
+}
