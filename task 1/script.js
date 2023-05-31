f6989e2db41e0e2726a70917973902fafc17ac2d
shablovskyi_illia <shablovskyiillia@gmail.com>
2023-05-31 18:36:25 +0300

    update task 1/script.js

---
+++ task 1/script.js
@@ -0,0 +1,9 @@
+// За допомогою prompt запитати як звуть користувача. За допомогою alert вивести "Hello, John! How are you?", де John це те, що ввів користувач
+
+let name = prompt("Enter your name:");
+
+if (parseInt(name)) {
+  alert("Please enter a name");
+} else {
+  alert(`Hello, ${name}! How are you?`);
+}
